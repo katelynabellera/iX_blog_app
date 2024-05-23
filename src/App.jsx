43af68c145b4da6b2304de7a258bf818912d63fb
katelynabellera <kabeller@umich.edu>
2024-05-23 08:30:13 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./Components/HomePage";
+/* import "bootstrap/dist/js/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/dotn/bootstrap-icons.css"; */
+
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
