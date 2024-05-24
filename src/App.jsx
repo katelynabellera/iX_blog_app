7144ead2bb99e4ecbe7c1ecf9e32190033fe511b
katelynabellera <kabeller@umich.edu>
2024-05-24 12:52:52 +0200

    day 5 in class changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./Components/HomePage";
@@ -7,15 +7,30 @@ import BlogsPage from "./Components/BlogsPage";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/dotn/bootstrap-icons.css"; */
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import { Link, useNavigate, useParams } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+  {
+    path: "/blogs",
+    element: <BlogsPage />,
+  },
+  // {
+  //   path: "/blogs/:categoryId?",
+  //   element: <BlogsPage />,
+  // },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <HomePage />
-      <BlogsPage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Components/BlogsPage/index.js
+++ src/Components/BlogsPage/index.js
@@ -1,52 +1,79 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Navbar from "../../Components/Navbar";
 import Heading from "../../Components/Heading";
 import BlogList from "../../Components/BlogList";
-// import CategoriesList from "../../Components/CategoriesList";
-import Footer from "../../Components/Footer";
+//import Footer from "../../Components/Footer";
 
 import "../../App.css";
-// import "./index.css";
+import "./index.css";
 
-// Week 1: Import the blogPosts and categories from the dummy-data.json file
+// Importing dummy data
 const data = require("../../dummy-data.json");
 let blogPosts = data.blogPosts;
 const categories = data.categories;
 
 export default function BlogsPage() {
+  //Initializing our states:
+  const [categoryId, setCategoryId] = useState();
+  const [blogs, setBlogs] = useState(blogPosts);
+ // const [displayBlogs, setDisplayBlogs] = useState(blogPosts);
+  
+  useEffect(
+    () => {
+    if (categoryId) {
+      const filterBlogs = blogPosts.filter((blog)=> {
+        return blog.categories.some((category)=>category.id === categoryId)
+      });
+      setBlogs(filterBlogs);
+    } 
+  }, 
+  [categoryId]
+);
 
-  const [categoryId, setCategoryId] = useState(undefined);
-  const [blogs, setBlogs] = useState([]);
 
-  useEffect(() => {
-    // Week 1: Filter the blogPosts based on the categoryId
-    const blogs = blogPosts.filter((x) =>
-      categoryId !== undefined
-        ? x.categories.find((y) => y.id.toString() === categoryId.toString())
-        : true
-    );
-    setBlogs(() => blogs);
-  }, [categoryId]);
+// return() => {
+//   //cleanup code
+// }
+
+  const CategoriesList = ( {categoryId} ) => {
+    return categories.map((category, index) => {
+      return categoryId === category.id ? (
+        <button
+          key={index}
+          onClick={() => setCategoryId(category.id)}
+          style={{ color: "blue" }}
+        >
+          <p key={index}>{category.title}</p>
+        </button>
+      ) : (
+        <button
+          key={index}
+          onClick={() => setCategoryId(category.id)}
+          style={{ color: "black" }}
+        >
+          <p key={index}>{category.title}</p>
+        </button>
+      );
+    });
+  };
 
   return (
     <>
       <Navbar />
       <div className="container">
         <Heading />
-        {/* <div className="scroll-menu"> */}
-          {/* <CategoriesList 
-              categories={categories} 
-              categoryId={categoryId}
-              setCategoryId={setCategoryId}>
-          </CategoriesList> */}
-        {/* </div> */}
+        <div className="scroll-menu">
+          <CategoriesList categoryId={categoryId}/>
+        </div>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <p className="page-subtitle">Blog Posts</p>
         </div>
         <BlogList blogPosts={blogs} />
+        
       </div>
       {/* <Footer /> */}
     </>
   );
+
 }
