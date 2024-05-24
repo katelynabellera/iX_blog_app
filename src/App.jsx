59389c4af6cf7cbc6d8ee1ae151b3e0d8f4a99da
katelynabellera <kabeller@umich.edu>
2024-05-24 11:02:26 +0200

    added blogspage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./Components/HomePage";
+import BlogsPage from "./Components/BlogsPage";
 /* import "bootstrap/dist/js/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/dotn/bootstrap-icons.css"; */
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <HomePage />
+      <BlogsPage />
     </div>
   );
 }
--- src/Components/BlogsPage/index.js
+++ src/Components/BlogsPage/index.js
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../Components/Navbar";
 import Heading from "../../Components/Heading";
 import BlogList from "../../Components/BlogList";
-import CategoriesList from "../../Components/CategoriesList";
+// import CategoriesList from "../../Components/CategoriesList";
 import Footer from "../../Components/Footer";
 
 import "../../App.css";
-import "./index.css";
+// import "./index.css";
 
 // Week 1: Import the blogPosts and categories from the dummy-data.json file
 const data = require("../../dummy-data.json");
@@ -34,19 +34,19 @@ export default function BlogsPage() {
       <Navbar />
       <div className="container">
         <Heading />
-        <div className="scroll-menu">
-          <CategoriesList 
+        {/* <div className="scroll-menu"> */}
+          {/* <CategoriesList 
               categories={categories} 
               categoryId={categoryId}
               setCategoryId={setCategoryId}>
-          </CategoriesList>
-        </div>
+          </CategoriesList> */}
+        {/* </div> */}
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <p className="page-subtitle">Blog Posts</p>
         </div>
         <BlogList blogPosts={blogs} />
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
